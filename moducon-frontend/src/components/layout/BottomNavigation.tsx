d4b5252e298372299bdf4688c76ce33ad140cd9b
hchang <cake0103@nate.com>
2025-12-01 17:41:57 +0900

    feat: QR 버튼 아이콘 크기 증가 (32px → 36px)
    - 버튼 내 아이콘 크기를 36px로 증가
    - stroke 색상 #666666 유지
    - 중앙 정렬 유지 (absolute + translate)
    - 관련 파일: src/components/layout/BottomNavigation.tsx

--- moducon-frontend/src/components/layout/BottomNavigation.tsx
+++ moducon-frontend/src/components/layout/BottomNavigation.tsx
@@ -45,8 +45,8 @@ export function BottomNavigation() {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
+              width="36"
+              height="36"
               viewBox="0 0 24 24"
               fill="none"
               stroke="#666666"
